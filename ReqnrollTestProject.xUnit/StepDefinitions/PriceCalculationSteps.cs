60d00d05de30a2b734da1ebd917256b08636628c
robson-jsouza <robson.jsouza@outlook.com>
2025-02-19 17:19:22 -0300

    Add files via upload

---
+++ ReqnrollTestProject.xUnit/StepDefinitions/PriceCalculationSteps.cs
@@ -0,0 +1,40 @@
+﻿using Reqnroll;
+using FluentAssertions;
+
+namespace ReqnrollTestProject.xUnit.StepDefinitions
+{
+    [Binding]
+    public class PriceCalculationSteps
+    {
+        private readonly PriceCalculator _priceCalculator = new();
+        private Dictionary<string, int> _basket = new();
+        private decimal _calculatedPrice;
+
+        [Given(@"the client started shopping")]
+        public void GivenTheClientStartedShopping()
+        {
+            _basket = new Dictionary<string, int>();
+        }
+
+        [Given(@"the client added (.*) pcs of ""(.*)"" to the basket")]
+        public void GivenTheClientAddedPcsOfToTheBasket(int quantity, string itemName)
+        {
+            if (_basket.ContainsKey(itemName))
+                _basket[itemName] += quantity;
+            else
+                _basket[itemName] = quantity;
+        }
+
+        [When(@"the basket is prepared")]
+        public void WhenTheBasketIsPrepared()
+        {
+            _calculatedPrice = _priceCalculator.CalculatePrice(_basket);
+        }
+
+        [Then(@"the basket price should be \$(.*)")]
+        public void ThenTheBasketPriceShouldBe(decimal expectedPrice)
+        {
+            _calculatedPrice.Should().Be(expectedPrice);
+        }
+    }
+}
